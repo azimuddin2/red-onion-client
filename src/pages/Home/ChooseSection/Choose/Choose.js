f2162a8c015e0940d3a3cdf4f5ddac87dd9e67ad
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-06-17 23:20:26 +0600

    updated a choose section

--- src/pages/Home/ChooseSection/Choose/Choose.js
+++ src/pages/Home/ChooseSection/Choose/Choose.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsArrowRightCircleFill } from 'react-icons/bs';
 import './Choose.css';
 
 const Choose = ({ choose }) => {
     const { image, name, description } = choose;
+    const [isReadMore, setIsReadMore] = useState(true);
+
+    const toggleReadMore = () => {
+        setIsReadMore(!isReadMore);
+    }
 
     return (
         <div className='col-4 choose'>
             <img src={image} alt="" />
-            <div>
+            <div className='choose-info'>
                 <h3>{name}</h3>
-                <p>{description}</p>
+                <p>
+                    {isReadMore ? description.slice(0, 100) : description}
+                    <span onClick={toggleReadMore}>
+                        {
+                            isReadMore ?
+                                <>
+                                    <span style={{ color: '#7ac948', fontWeight: '500', marginRight: '5px' }}>...read more</span>
+                                    <BsArrowRightCircleFill style={{ color: '#7ac948' }}></BsArrowRightCircleFill>
+                                </>
+                                :
+                                <>
+                                    <span style={{ color: '#7ac948', fontWeight: '500', marginRight: '5px' }}>show less</span>
+                                    <BsArrowRightCircleFill style={{ color: '#7ac948' }}></BsArrowRightCircleFill>
+                                </>
+                        }
+                    </span>
+                </p>
             </div>
         </div>
     );
