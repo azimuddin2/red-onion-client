5341ae92109a72654d4de8af6156787940deb6ec
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-07-08 22:17:59 +0600

    added firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiNxMMG3VZ_cDe3FPWWLWfqdNmCbtVniY",
+  authDomain: "red-onion-ef492.firebaseapp.com",
+  projectId: "red-onion-ef492",
+  storageBucket: "red-onion-ef492.appspot.com",
+  messagingSenderId: "379387577623",
+  appId: "1:379387577623:web:0eb07d13bb859c004ae352"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
